2b7b6e7939d6a70016dfcf36d0eec363fcc27a1e
Simon Andrews <Andrewss@babraham.ac.uk>
2017-01-08 15:56:18 +0000

    Added string methods to model

--- courses/models.py
+++ courses/models.py
@@ -4,9 +4,15 @@ class Duration(models.Model):
 	length_text = models.CharField(max_length=200)
 	hours = models.IntegerField(default=0)
 	current_cost_pence = models.IntegerField(default=0)
+	
+	def __str__(self):
+		return self.length_text
 
 class Course(models.Model):
 	title = models.CharField(max_length=200)
 	description = models.TextField()
 	duration = models.ForeignKey(Duration,on_delete=models.PROTECT)
 	currently_available = models.BooleanField(default=True)
+	
+	def __str__(self):
+		return self.title
