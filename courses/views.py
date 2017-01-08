82a4142e73aa03279d374159027f460bef9aa8d9
Simon Andrews <Andrewss@babraham.ac.uk>
2017-01-08 17:08:18 +0000

    Created view of all courses

--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+from .models import *
 
-def index(request):
-	return HttpResponse("This will be a list of courses")
+class CoursesView(generic.ListView):
+	template_name = 'courses/courses.html'
+	context_object_name = 'course_list'
+	
+	def get_queryset(self):
+		return Course.objects.all()
+
+class DiaryView(generic.ListView):
+	pass
+
+class CourseView(generic.DetailView):
+	model = Course
+	template_name = 'courses/course.html'
+
