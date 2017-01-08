b21b111b9a9366b975900982cc52541ab78b6998
Simon Andrews <Andrewss@babraham.ac.uk>
2017-01-08 15:26:05 +0000

    Created basic model

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Duration(models.Model):
+	length_text = models.CharField(max_length=200)
+	hours = models.IntegerField(default=0)
+	current_cost_pence = models.IntegerField(default=0)
+
+class Course(models.Model):
+	title = models.CharField(max_length=200)
+	description = models.TextField()
+	duration = models.ForeignKey(Duration,on_delete=models.PROTECT)
+	currently_available = models.BooleanField(default=True)
