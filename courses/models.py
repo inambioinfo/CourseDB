4fb249f5ef43652a192470876d270c1b42d11c57
s-andrews <simon@proteo.me.uk>
2017-01-09 20:08:42 +0000

    Added a booking object

--- courses/models.py
+++ courses/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Duration(models.Model):
 	length_text = models.CharField(max_length=200)
@@ -23,6 +24,18 @@ class CourseInstance(models.Model):
 	end_datetime = models.DateTimeField()
 	site = models.CharField(max_length=200)
 	location = models.CharField(max_length=200)
+	spaces = models.IntegerField(default=0)
+	
+	def is_full(self):
+		return len(self.booking_set.all()) >= self
 	
 	def __str__(self):
 		return self.course.title+":"+str(self.start_datetime)
+
+class Booking(models.Model):
+	course_instance = models.ForeignKey(CourseInstance, on_delete=models.PROTECT)
+	person = models.ForeignKey(User, on_delete=models.PROTECT)
+	date_booked = models.DateTimeField()
+	
+	def __str__(self):
+		return self.user.email+" on "+self.course_instance.course.title
