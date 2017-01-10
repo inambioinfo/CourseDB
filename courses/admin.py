bbe8de440f59ec50c2277f4b9486596b89ebbda8
s-andrews <simon@proteo.me.uk>
2017-01-10 20:40:06 +0000

    Registerd the course elements with the admin interface

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Course, CourseInstance, Duration
+
+admin.site.register(Course)
+admin.site.register(CourseInstance)
+admin.site.register(Duration)
