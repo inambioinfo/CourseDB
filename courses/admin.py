b5bc9c0ec2d0740627b8caac78c257e47161ef9a
s-andrews <simon@proteo.me.uk>
2017-01-11 08:16:25 +0000

    Added learning objectives to the admin interface

--- courses/admin.py
+++ courses/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Course, CourseInstance, Duration
+from .models import Course, CourseInstance, Duration, LearningObjective
 
 admin.site.register(Course)
+admin.site.register(LearningObjective)
 admin.site.register(CourseInstance)
 admin.site.register(Duration)
