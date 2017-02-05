be12673f9058be1375838d46d3cd81a7b7d020c9
Simon Andrews <Andrewss@babraham.ac.uk>
2017-02-05 19:13:51 +0000

    Made the course details show only future dates

--- courses/urls.py
+++ courses/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.CoursesView.as_view(), name='courses'),
 	url(r'^diary/$', views.DiaryView.as_view(), name='diary'),
-	url(r'^course(?P<pk>[0-9]+)/$', views.CourseView.as_view(), name='course'),
+	url(r'^course(?P<course_id>[0-9]+)/$', views.CourseView, name='course'),
 	url(r'^instance(?P<pk>[0-9]+)/$', views.InstanceView.as_view(), name='instance'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -20,9 +20,20 @@ class DiaryView(generic.ListView):
 	def get_queryset(self):
 		return CourseInstance.objects.filter(start_datetime__gte=timezone.now())
 	
-class CourseView(generic.DetailView):
-	model = Course
-	template_name = 'courses/course.html'
+	
+def CourseView(request, course_id):
+	course = get_object_or_404(Course,pk=course_id)
+	instances = course.courseinstance_set.filter(start_datetime__gte=timezone.now())
+	
+	return render(request, "courses/course.html", {
+			'course': course,
+			'instances': instances
+	})
+	
+	
+#class CourseView(generic.DetailView):
+#	model = Course
+#	template_name = 'courses/course.html'
 
 class InstanceView(generic.DetailView):
 	model = CourseInstance
